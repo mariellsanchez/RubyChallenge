6d659e0c6b433913107814dd5d28e8e373e11243
Mariell <mariellsanchez@estudiantec.cr>
2022-11-14 11:15:11 -0600

    Task 1
    Given and URL, returns the shortcode (ID)

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -4,12 +4,53 @@ class ShortUrlsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-  end
-
-  def create
+    # need to fix here
+    @short_urls = ShortUrl.all 
+    render json: {
+      status: "SUCCESS",
+      message: 'Loaded data',
+      data: @short_urls
+    },
+    status: :ok
   end
 
   def show
+    @single_url = ShortUrl.find(params[:id])
+    render json: {
+      status: "SUCCESS",
+      message: 'Loaded short url',
+      data: @single_url
+    },
+    status: :ok
   end
 
+  def create
+    @short_url = ShortUrl.new(short_url_params)
+
+    if @short_url.save
+      render json: {
+        status: "SUCCESS",
+        message: 'Short url saved',
+        short_code: @short_url.id,
+        data: @short_url
+      },
+      status: :created
+    else
+      render json: {
+        status: "ERROR",
+        message: 'Short url NOT saved',
+        data: @short_url.errors
+      },
+      status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def short_url_params
+    # the only params we are going to permit
+    params.permit(:full_url)
+  end
+
+  
 end
--- spec/controllers/short_urls_controller_spec.rb
+++ spec/controllers/short_urls_controller_spec.rb
@@ -15,7 +15,6 @@ RSpec.describe ShortUrlsController, type: :controller do
 
     it "has a list of the top 100 urls" do
       get :index, format: :json
-
       expect(parsed_response['urls']).to be_include(short_url.public_attributes)
     end
 
